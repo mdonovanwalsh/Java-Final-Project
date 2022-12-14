27bee505c0fb9aeb57eab6ded5bba6cf92cbe3bd
mdonovanwalsh <104364829+mdonovanwalsh@users.noreply.github.com>
2022-12-14 16:06:34 -0400

    Add files via upload

---
+++ FinalProject/src/java/db/ConnectionParameters.java
@@ -0,0 +1,15 @@
+package db;
+
+/**
+ * It's better practice to store this information in a configuration file.
+ */
+public class ConnectionParameters {
+    
+    public static final String URL = "jdbc:derby://localhost:1527/staffdb";
+    public static final String USERNAME = "app";
+    public static final String PASSWORD = "app";
+    
+    // no instantiation allowed
+    private ConnectionParameters() {}
+    
+} // end class ConnectionParameters
