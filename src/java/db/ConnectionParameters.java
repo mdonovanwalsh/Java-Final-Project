4d684f4d6ae547d6dbe651e08e7bf3ed933e6b6f
mdonovanwalsh <mdonovanwalsh@gmail.com>
2022-12-14 17:16:24 -0400

    Added index.html

---
+++ src/java/db/ConnectionParameters.java
@@ -0,0 +1,15 @@
+package db;
+
+/**
+ * It's better practice to store this information in a configuration file.
+ */
+public class ConnectionParameters {
+    
+    public static final String URL = "jdbc:mysql://localhost:3306/quizappdb";
+    public static final String USERNAME = "app";
+    public static final String PASSWORD = "app";
+    
+    // no instantiation allowed
+    private ConnectionParameters() {}
+    
+} // end class ConnectionParameters
